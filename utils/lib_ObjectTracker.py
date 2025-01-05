1fd0b00f3a64acc96aebf38b2d5ef1e8a9ce8b69
ack00gar <arnaud.chevreux@gmail.com>
2025-01-05 16:08:34 +0100

    Interface simplifation, and added the frame_id to the debug data for display in video debug mode

--- utils/lib_ObjectTracker.py
+++ utils/lib_ObjectTracker.py
@@ -6,7 +6,7 @@ import math
 import logging
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 class LockedPenisBox:
     """
